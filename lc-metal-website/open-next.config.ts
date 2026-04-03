c15a7d6f4d0c9ac2f49ebee8357ec8b470f3cfcb
zhiyong ge <zhiyong.ge@pacteraedge.com>
2026-04-03 18:09:12 +0800

    feat: add middleware in OpenNextConfig

--- lc-metal-website/open-next.config.ts
+++ lc-metal-website/open-next.config.ts
@@ -2,10 +2,26 @@ import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
 const config: OpenNextConfig = {
   default: {
-    runtime: "edge",
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy" | function,
+      tagCache: "dummy" | function,
+      queue: "dummy" | "direct" | function,
+    },
   },
+  edgeExternals: ["node:crypto"],
   middleware: {
     external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy" | function,
+      tagCache: "dummy" | function,
+      queue: "dummy" | "direct" | function,
+    },
   },
 };
 
